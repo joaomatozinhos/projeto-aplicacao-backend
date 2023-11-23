39d3edd9dc600ac774747c95697fc60aa1fb4952
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-11-23 07:03:26 -0300

    criacao diretorios e estrutura front e backend

---
+++ backend/src/test/java/br/com/puc/callcenter/CallcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.puc.callcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
