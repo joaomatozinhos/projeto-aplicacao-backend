95c28c63b765e80ffa2c0d35d4d9afb12c4387ad
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-11-28 06:16:40 -0300

    simular busca clientes

--- frontend/src/common/model/Endereco.ts
+++ frontend/src/common/model/Endereco.ts
@@ -2,7 +2,7 @@ export interface Endereco {
   cep: string;
   logradouro: string;
   bairro: string;
-  complemento?: string;
+  complemento?: string | null;
   numero: string;
   uf: string;
   cidade: string;
